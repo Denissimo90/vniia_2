cddaad9c33988d866f6c8f0c0fa8aad0d4832be0
user11 <artem0gorlov@gmail.com>
2022-05-24 17:44:30 +0500

    ActionDto

--- WebApplication1/App.Entities/ActionDto.cs
+++ WebApplication1/App.Entities/ActionDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App.Entities.Dto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace App.Entities
 {
     public class ActionDto
     {
+        public string ActionId { get; set; }
+        public string ActionName { get; set; }
+        public List<ParticipantDto> ParticipantDtos { get; set; }
+        public List<TeamDto> TeamDtos { get; set; }
+
     }
 }
